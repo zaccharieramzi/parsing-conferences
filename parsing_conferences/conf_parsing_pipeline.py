22e4379bc0b56ee11037443444e32bdb96c0442d
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-06-07 22:22:25 +0200

    made the conf parsing pipeline more resilient

--- parsing_conferences/conf_parsing_pipeline.py
+++ parsing_conferences/conf_parsing_pipeline.py
@@ -7,8 +7,7 @@ from parsing_conferences.get_papers import get_neurips_papers
 if __name__ == '__main__':
     year = 2020
     neurips_generator = get_neurips_papers(year)
-    affiliation_data = []
-    n_samples = 10
+    n_samples = None
     counter = 0
     try:
         df_affiliations = pd.read_csv('affiliations_neurips_2020.csv', index_col=0)
@@ -17,6 +16,7 @@ if __name__ == '__main__':
     for neurips_art, art_link in tqdm(neurips_generator):
         if df_affiliations is not None and art_link in df_affiliations['article_link'].unique():
             continue
+        affiliation_data = []
         affiliations = get_affiliations('tmp.pdf')
         for affiliation in affiliations:
             affiliation_data.append({
@@ -27,10 +27,10 @@ if __name__ == '__main__':
         counter += 1
         if n_samples is not None and counter > n_samples:
             break
-    df_affiliations_new = pd.DataFrame(affiliation_data)
-    if df_affiliations is not None:
-        df_affiliations = df_affiliations.append(df_affiliations_new)
-    else:
-        df_affiliations = df_affiliations_new
-    df_affiliations.to_csv('affiliations_neurips_2020.csv')
+        df_affiliations_new = pd.DataFrame(affiliation_data)
+        if df_affiliations is not None:
+            df_affiliations = df_affiliations.append(df_affiliations_new)
+        else:
+            df_affiliations = df_affiliations_new
+        df_affiliations.to_csv('affiliations_neurips_2020.csv')
     print(df_affiliations)
