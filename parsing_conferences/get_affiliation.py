d21f53ba9cf1ea53f55c3551265ecde839dd0280
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-06-08 15:16:41 +0200

    added an async version to get affiliations

--- parsing_conferences/get_affiliation.py
+++ parsing_conferences/get_affiliation.py
@@ -4,15 +4,32 @@ from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
 
+
+CERMINE_URL = 'http://cermine.ceon.pl/extract.do'
+
 def get_affiliations(pdf_path):
     with open(pdf_path, 'rb') as f:
         data = f.read()
     response = requests.post(
-        'http://cermine.ceon.pl/extract.do',
+        CERMINE_URL,
         headers={'Content-Type': 'application/binary'} ,
         data=data,
     )
     xml_pdf = response.content
+    soup = BeautifulSoup(xml_pdf, 'lxml')
+    institutions = [i.contents[0] for i in soup.find_all('institution')]
+    return institutions
+
+def get_affiliations_async(pdf_path, session):
+    with open(pdf_path, 'rb') as f:
+        data = f.read()
+    resp = await session.post(
+        CERMINE_URL,
+        data=data,
+        headers={'Content-Type': 'application/binary'},
+    )
+    resp.raise_for_status()
+    xml_pdf = await resp.text()
     soup = BeautifulSoup(xml_pdf, 'lxml')
     institutions = [i.contents[0] for i in soup.find_all('institution')]
     return institutions
