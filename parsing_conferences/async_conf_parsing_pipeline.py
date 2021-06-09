7904da753e86694777ff2645d56f8a974a7ef991
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-06-09 10:32:18 +0200

    working async conf pipe

--- parsing_conferences/async_conf_parsing_pipeline.py
+++ parsing_conferences/async_conf_parsing_pipeline.py
@@ -1,7 +1,9 @@
 import asyncio
 import csv
 from pathlib import Path
+import time
 
+import aiohttp
 from aiohttp import ClientSession
 import pandas as pd
 
@@ -11,24 +13,32 @@ from parsing_conferences.get_papers import get_neurips_papers_batched
 OUT_FILE = 'async_affiliations_neurips_{year}.csv'
 
 async def write_affiliations(session, out_filename, neurips_art, art_link, pdf_path='tmp.pdf'):
+    start = time.time()
     affiliations = await get_affiliations_async(pdf_path, session)
-    for affiliation in affiliations:
-        affiliation_data = {
-            'affiliation': affiliation,
-            'article_link': art_link,
-            'paper_title': neurips_art,
-        }
-        outfile_exists = Path(out_filename).is_file()
-        with open(out_filename, 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=list(affiliation_data.keys()))
-            if not outfile_exists:
-                writer.writeheader()
+    end = time.time()
+    print(pdf_path, end - start, start)
+    outfile_exists = Path(out_filename).is_file()
+    with open(out_filename, 'a') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=[
+            'affiliation',
+            'article_link',
+            'paper_title',
+        ])
+        if not outfile_exists:
+            writer.writeheader()
+        for affiliation in affiliations:
+            affiliation_data = {
+                'affiliation': affiliation,
+                'article_link': art_link,
+                'paper_title': neurips_art,
+            }
             writer.writerow(affiliation_data)
+    Path(pdf_path).unlink()
 
 async def bulk_get_affil_and_write(df_affiliations, batch_id=0, batch_size=10, year=2020):
-    async with ClientSession() as session:
+    neurips_generator = get_neurips_papers_batched(year, batch_size, batch_id)
+    async with ClientSession(raise_for_status=True, timeout=aiohttp.ClientTimeout(batch_size*50)) as session:
         tasks = []
-        neurips_generator = get_neurips_papers_batched(year, batch_size, batch_id)
         for neurips_art, art_link, pdf_name in neurips_generator:
             if df_affiliations is not None and art_link in df_affiliations['article_link'].unique():
                 continue
@@ -52,7 +62,7 @@ if __name__ == '__main__':
         df_affiliations = None
     batch_size = 10
     batch_id = 0
-    max_batch = 10000
+    max_batch = 1
     while batch_id < max_batch:
         try:
             asyncio.run(bulk_get_affil_and_write(df_affiliations, batch_id, batch_size, year))
